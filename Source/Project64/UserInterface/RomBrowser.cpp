ccef1200215b62ebbea7e04115c65bfd6e570676
Loyla <159725294+Loyla486@users.noreply.github.com>
2024-10-25 00:12:30 -0700

    Update RomBrowser.cpp
    Removed "if (UISettingsLoadBool(UserInterface_ShowingNagWindow)) {return;}"

--- Source/Project64/UserInterface/RomBrowser.cpp
+++ Source/Project64/UserInterface/RomBrowser.cpp
@@ -800,11 +800,6 @@ void CRomBrowser::RomList_OpenRom(uint32_t /*pnmh*/)
     }
     m_StopRefresh = true;
 
-    if (UISettingsLoadBool(UserInterface_ShowingNagWindow))
-    {
-        return;
-    }
-
     if ((CPath(pRomInfo->szFullFileName).GetExtension() != "ndd") && (CPath(pRomInfo->szFullFileName).GetExtension() != "d64"))
         CN64System::RunFileImage(pRomInfo->szFullFileName);
     else
